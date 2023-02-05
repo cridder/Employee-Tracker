8268384b4aea401fde44ccc6e4c5f1879e618842
cridder <cjridder@gmail.com>
2023-02-05 15:43:45 -0800

    running

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+const express = require("express");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-require("dotenv").config();
-const db = mysql.createConnection({
-	host: "localhost",
-	port: 3001,
-	user: "root",
-	database: "et_db",
-});
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+	{
+		host: "localhost",
+		// MySQL username,
+		user: "root",
+		// MySQL password
+		password: "",
+		database: "et_db",
+	},
+	console.log(`Connected to the et_db database.`)
+);
 
 function selectQuestion() {
 	inquirer
@@ -243,3 +256,12 @@ function exit() {
 }
 
 selectQuestion();
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+	res.status(404).end();
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
